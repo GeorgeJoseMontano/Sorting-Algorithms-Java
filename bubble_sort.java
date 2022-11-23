493da0d325580332515ef1ed2d67e88bb580f4dc
George Jose Montano <yoshilute.248@gmail.com>
2022-11-24 00:22:17 +0800

    bubble sort commit

---
+++ bubble_sort.java
@@ -0,0 +1,36 @@
+import java.util.Random;
+
+public class bubble_sort {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        int[] array = new int[10];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = rand.nextInt(100);
+        }
+        System.out.println("Unsorted array:");
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+        bubbleSort(array);
+        System.out.println("Sorted array:");
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void bubbleSort(int[] array) {
+        int temp;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 1; j < (array.length - i); j++) {
+                if (array[j - 1] > array[j]) {
+                    temp = array[j - 1];
+                    array[j - 1] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+    }
+}
+
